bbad9802dca3743ec20414581549176204557f15
AnYuan <donganyuan1010@gmail.com>
2016-10-10 00:23:13 +0800

    add some leetcode questions

--- AlgorithmsInSwift.playground/Pages/2Sum.xcplaygroundpage/Contents.swift
+++ AlgorithmsInSwift.playground/Pages/2Sum.xcplaygroundpage/Contents.swift
@@ -5,3 +5,29 @@ import Foundation
 var str = "Hello, playground"
 
 //: [Next](@next)
+
+/*
+ Given nums = [2, 7, 11, 15], target = 9,
+ 
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ 
+ UPDATE (2016/2/13):
+ The return format had been changed to zero-based indices. Please read the above updated description carefully.
+ https://leetcode.com/problems/two-sum/
+ */
+
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dic = [Int:Int]()
+    for i in 0 ..< nums.count {
+        let complement = target - nums[i]
+        if dic[complement] != nil {
+            return [dic[complement]!, i]
+        }
+        dic[nums[i]] = i
+    }
+    return [0,0]
+}
+
+twoSum([2, 7, 11, 15], 9)
