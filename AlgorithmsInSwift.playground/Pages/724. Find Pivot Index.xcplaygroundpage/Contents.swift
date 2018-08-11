d4ab5cd38aad1a4ffceac27e18d8a73cd7bfc1ce
anyuan dong <donganyuan@didichuxing.com>
2018-08-11 18:52:35 +0800

    724. Find Pivot Index

---
+++ AlgorithmsInSwift.playground/Pages/724. Find Pivot Index.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+class Solution {
+    func pivotIndex(_ nums: [Int]) -> Int {
+        
+        guard nums.count > 1 else { return nums.isEmpty ? -1 : 0 }
+        
+        let sum = nums.reduce(0, +)
+        
+        if sum - nums[0] == 0 { return 0 }
+        
+        var leftTotal = 0
+        for i in 1..<nums.count {
+            leftTotal += nums[i-1]
+            if (sum - leftTotal - nums[i]) == leftTotal {
+                return i;
+            }
+        }
+        
+        return -1
+    }
+}
+
+
+let s = Solution()
+//[1, 7, 3, 6, 5, 6]
+//s.pivotIndex([-1,-1,-1,0,1,1])
+//s.pivotIndex([1, 7, 3, 6, 5, 6])
+//s.pivotIndex([-1,-1,0,1,0,-1])
+//s.pivotIndex([-1,-1,0,1,1,0])
+s.pivotIndex([-1,-1,1,1,0,0])
