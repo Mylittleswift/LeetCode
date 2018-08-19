52322a8598fa858e02f2deae734b216fc1649e34
AnYuan <donganyuan1010@gmail.com>
2018-08-19 16:20:15 +0800

    118. Pascal's Triangle

---
+++ AlgorithmsInSwift.playground/Pages/118. Pascal's Triangle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/pascals-triangle/description/
+
+import Foundation
+
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        
+        guard numRows > 0 else {
+            return [[Int]]()
+        }
+        
+        //init matrix
+        var pascal = Array(repeating: [Int](), count: numRows)
+        for i in 0 ... numRows - 1 {
+            if i == 0 {
+                pascal[i] = [1]
+            } else if i == 1 {
+                pascal[i] = [1, 1]
+            } else {
+                var temp = [Int]()
+                temp.append(1)
+                for j in 1 ... i - 1 {
+                    temp.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+                }
+                temp.append(1)
+                pascal[i] = temp
+            }
+        }
+        
+        return pascal
+    }
+}
+
+let s = Solution()
+s.generate(5)
