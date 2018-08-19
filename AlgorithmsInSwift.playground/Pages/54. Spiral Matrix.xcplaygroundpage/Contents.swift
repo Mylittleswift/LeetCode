6af13e25b12d7d6bc8ab7ca27677699222044eff
AnYuan <donganyuan1010@gmail.com>
2018-08-19 11:25:20 +0800

    update 54. Spiral Matrix answer

--- AlgorithmsInSwift.playground/Pages/54. Spiral Matrix.xcplaygroundpage/Contents.swift
+++ AlgorithmsInSwift.playground/Pages/54. Spiral Matrix.xcplaygroundpage/Contents.swift
@@ -1,82 +1,60 @@
 import Foundation
+import XCTest
 
 class Solution {
     func spiralOrder(_ matrix: [[Int]]) -> [Int] {
         
-        guard matrix.count > 0 else {
-            return []
-        }
-        guard matrix[0].count > 0 else {
+        guard matrix.count > 0,
+            matrix[0].count > 0 else {
             return []
         }
         
         let col = matrix[0].count
         let row = matrix.count
         
-        var upperLeft = (1,0)
-        var upperRight = (0, col-1)
-        var lowerLeft = (row-1, 0)
-        var lowerRight = (row-1, col-1)
-        
-        var step = 0
-        let total = col * row
         var spiral = [Int]()
+
+        var r = 0, c = 0
         
-        func checkFinish() -> Bool {
-            return step >= total
-        }
-        
-        while true {
+        while (r < (row + 1) / 2) && (c < (col + 1) / 2) {
+            //top
+            for i in c ..< col - c {
+                spiral.append(matrix[r][i])
+            }
+            
             //right
-            for i in upperLeft.1 ..< upperRight.1 + 1 {
-                spiral.append(matrix[upperRight.0][i])
-                step += 1
+            for i in r + 1 ..< row - r {
+                spiral.append(matrix[i][col - c - 1])
             }
-            if checkFinish() { break }
-            
-            //down
-            for i in upperRight.0+1 ..< lowerRight.0 + 1{
-                spiral.append(matrix[i][lowerRight.1])
-                step += 1
+            //bottom
+            var i = col - c - 2
+            while row - r - 1 > r && i >= c {
+                spiral.append(matrix[row - r - 1][i])
+                i -= 1
             }
-            if checkFinish() { break }
-
             //left
-            for i in stride(from: lowerRight.1 - 1, to: lowerLeft.1-1, by: -1) {
-                spiral.append(matrix[lowerRight.0][i])
-                step += 1
+            i = row - r - 2
+            while (col - c - 1 > c && i > r) {
+                spiral.append(matrix[i][c])
+                i -= 1
             }
 
-            if checkFinish() { break }
-
-            //up
-            for i in stride(from: lowerLeft.0-1, to: upperLeft.0-1, by: -1) {
-                spiral.append(matrix[i][lowerLeft.1])
-                step += 1
-            }
-
-            if checkFinish() { break }
-            
-            upperLeft = (upperLeft.0 + 1, upperLeft.1 + 1)
-            upperRight = (upperRight.0 + 1, upperRight.1 - 1)
-            lowerLeft = (lowerLeft.0 - 1, lowerLeft.1 + 1)
-            lowerRight = (lowerRight.0 - 1, lowerRight.1 - 1)
+            r += 1
+            c += 1
         }
         
         return spiral
     }
 }
 
-
 let s = Solution()
 
-assert(s.spiralOrder([]) == [])
+s.spiralOrder([])
 
-assert(s.spiralOrder([[1,2,3],
-                      [4,5,6],
-                      [7,8,9]]) == [1,2,3,6,9,8,7,4,5])
+s.spiralOrder([[1,2,3],
+               [4,5,6],
+               [7,8,9]])
 
-assert(s.spiralOrder([[1,2,3,4],
-                      [5,6,7,8],
-                      [9,10,11,12]]) == [1,2,3,4,8,12,11,10,9,5,6,7])
-
+s.spiralOrder([[1,2,3,4],
+               [5,6,7,8],
+               [9,10,11,12]])
