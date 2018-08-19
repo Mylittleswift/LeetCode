4930de78035dad6515b49719df74d078e858979a
AnYuan <donganyuan1010@gmail.com>
2018-08-19 15:31:40 +0800

    59. Spiral Matrix II

---
+++ AlgorithmsInSwift.playground/Pages/59. Spiral Matrix II.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//https://leetcode.com/problems/spiral-matrix-ii/description/
+
+import Foundation
+
+class Solution {
+    func generateMatrix(_ n: Int) -> [[Int]] {
+        //create N x N matrix
+        var spiral = Array(repeating:
+            Array(repeating: 0, count: n),
+                           count: n)
+        
+        let col = n, row = n
+        var c = 0, r = 0
+        var cnt = 1
+        
+        while (r < (row + 1) / 2) && (c < (col + 1) / 2) {
+            //top
+            for i in c ..< col - c {
+                spiral[r][i] = cnt
+                cnt += 1
+            }
+            
+            //right
+            for i in r + 1 ..< row - r {
+                spiral[i][col - c - 1] = cnt
+                cnt += 1
+            }
+            //bottom
+            var i = col - c - 2
+            while row - r - 1 > r && i >= c {
+                spiral[row - r - 1][i] = cnt
+                cnt += 1
+                i -= 1
+            }
+            //left
+            i = row - r - 2
+            while (col - c - 1 > c && i > r) {
+                spiral[i][c] = cnt
+                cnt += 1
+                i -= 1
+            }
+            
+            r += 1
+            c += 1
+        }
+        return spiral
+    }
+}
+
+
+let s = Solution()
+s.generateMatrix(3)
+
