444416e8909e2d00d91c4e4ea641078cf6af5dca
Anyuan Dong <donganyuan1010@gmail.com>
2019-04-09 18:41:26 +0800

    add dummy linkedlist code

---
+++ Algorithms-in-Swift.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+example(of: "push") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print(list)
+}
+
+
+example(of: "append") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    
+    print(list)
+}
+
+example(of: "inserting at a particular index") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Before inserting: \(list)")
+    var middleNode = list.node(at: 1)!
+    for _ in 1...4 {
+        middleNode = list.insert(-1, after: middleNode)
+    }
+    print("After inserting: \(list)")
+}
+
+example(of: "pop") {
+    var list = LinkedList<Int>()
+    list.push(1)
+    list.push(2)
+    list.push(3)
+    
+    print("Before popping list: \(list)")
+    let popedValue = list.pop()
+    print("After popping list: \(list)")
+    print("Popped value: " + String(describing: popedValue))
+}
+
+example(of: "removeLast") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Before removing last node: \(list)")
+    let popedValue = list.removeLast()
+    print("After removing last node: \(list)")
+    print("Removed value: " + String(describing: popedValue))
+}
+
+example(of: "removing a node after a particular node") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Before removing at particular index: \(list)")
+    let index = 1
+    let node = list.node(at: index - 1)!
+    let removeValue = list.remove(after: node)
+    
+    print("After removing at particular index: \(list)")
+    print("Removed value: " + String(describing: removeValue))
+}
