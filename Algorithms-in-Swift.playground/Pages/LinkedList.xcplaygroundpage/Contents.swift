3985789e3797b40a0f57d5dc1e69362ef0bf04b5
Anyuan Dong <donganyuan1010@gmail.com>
2019-04-12 17:10:26 +0800

    [LinkedList] add Challenges

--- Algorithms-in-Swift.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
+++ Algorithms-in-Swift.playground/Pages/LinkedList.xcplaygroundpage/Contents.swift
@@ -101,3 +101,80 @@ example(of: "linked list cow") {
     print("List1: \(list1)")
     print("List2: \(list2)")
 }
+
+func printInReverse<T>(_ list: LinkedList<T>) {
+    printNode(list.head)
+}
+
+private func printNode<T> (_ node: Node<T>?) {
+    guard let node = node else { return }
+    printNode(node.next)
+    print(node.value)
+}
+
+example(of: "prints out in reverse order") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Original list: \(list)")
+    print("Printing in reverse:")
+    printInReverse(list)
+}
+
+
+func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
+    var slow = list.head
+    var fast = list.head
+    
+    while let nextFast = fast?.next {
+        fast = nextFast.next
+        slow = slow?.next
+    }
+    
+    return slow
+}
+
+example(of: "getting the middle node") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print(list)
+    
+    if let middleNode = getMiddle(list) {
+        print(middleNode)
+    }
+}
+
+
+extension LinkedList {
+    
+    mutating func reverse() {
+        tail = head
+        var prev = head
+        var current = head?.next
+        prev?.next = nil
+        
+        while current != nil {
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        head = prev
+    }
+}
+
+example(of: "reversing a list") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print("Original list: \(list)")
+    list.reverse()
+    print("Reversed list: \(list)")
+}
