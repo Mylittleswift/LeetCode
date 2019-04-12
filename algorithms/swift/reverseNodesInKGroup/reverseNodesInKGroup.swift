3ce2f5a4e10415104fda631e9a8d3068348f0433
Anyuan Dong <donganyuan1010@gmail.com>
2019-04-12 14:54:08 +0800

    [25 Reverse Nodes in k-Group] update implementation
    Using recursive method

--- algorithms/swift/reverseNodesInKGroup/reverseNodesInKGroup.swift
+++ algorithms/swift/reverseNodesInKGroup/reverseNodesInKGroup.swift
@@ -1,3 +1,4 @@
+
 /*
  Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
 
@@ -19,38 +20,19 @@ func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
     guard let _ = head else { return head }
     guard k > 1 else { return head }
 
-    var head = head
-    var current = head
-    var prev: ListNode?
+    let (first, last) = divide(head: head, length: k)
+    guard let _ = last else { return first }
 
-    // traverse list
-    while current?.next != nil {
-        let (first, last) = divide(head: current, length: k)
-        current = last?.next
-        // there are k nodes
-
-        guard let _ = last else { return head }
-
-        let (r_first, r_last) = reverse(head: first, tail: last)
-
-        // reset head
-        if r_last === head {
-            head = r_first
-            prev = nil
-        }
-
-        prev?.next = r_first
-        r_last?.next = current
-
-        prev = r_last
-    }
-    return head
+    let current = last?.next
+    let (r_first, r_last) = reverse(head: first, tail: last)
+    r_last?.next = reverseKGroup(current, k)
+    return r_first
 }
 
 func divide(head: ListNode?, length: Int) -> (ListNode?, ListNode?) {
     let first = head
     var last = head
-    for _ in 1...length-1 {
+    for _ in 1..<length {
         let temp = last?.next
         last = temp
         if temp == nil { break }
