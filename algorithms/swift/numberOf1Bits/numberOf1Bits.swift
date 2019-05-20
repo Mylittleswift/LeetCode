eaaef1e17111fed21ee771da1a3ff6e82945e124
Anyuan Dong <donganyuan1010@gmail.com>
2019-05-20 08:09:59 +0800

    add comment

--- algorithms/swift/numberOf1Bits/numberOf1Bits.swift
+++ algorithms/swift/numberOf1Bits/numberOf1Bits.swift
@@ -3,7 +3,7 @@
  */
 func hammingWeight(_ n: UInt) -> UInt {
     var count: UInt = 0
-    var n = n
+    var n = n // 0b0000111101
     while n != 0 {
         count = count + (a & 1)
         n = n >> 1
