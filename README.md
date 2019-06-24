ba4cd325ca6b363f2e1caebc30fa13b80fc9bae0
Anyuan Dong <donganyuan1010@gmail.com>
2019-06-24 09:20:19 +0800

    [Leetcode]215. Kth Largest Element in an Array
    add swift implementation

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ Please feel free to send **Pull Request**, **Issue** or give this repo a big **S
 |206|[Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/) | [Java](./algorithms/java/reverseLinkedList/reverseLinkedList.java), [Swift](./algorithms/swift/reverseLinkedList/reverseLinkedList1.swift), [Swift](./algorithms/swift/reverseLinkedList/reverseLinkedList0.swift)|Easy|
 |208|[Implement Trie (Prefix Tree)](https://leetcode.com/problems/implement-trie-prefix-tree/) | [Swift](./algorithms/swift/implementTrie/implementTrie.swift)|Medium|
 |212|[Word Search II](https://leetcode.com/problems/word-search-ii/) | [Swift](./algorithms/swift/wordSearchII/wordSearchII.swift)|Hard|
+|215|[Kth Largest Element in an Array](https://leetcode.com/problems/kth-largest-element-in-an-array/) | [Swift](./algorithms/swift/kthLargestElementInAnArray/kthLargestElementInAnArray.swift)|Medium|
 |224|[Basic Calculator](https://leetcode.com/problems/basic-calculator/) | [Swift](./algorithms/swift/basicCalculator/basicCalculator.swift) |Hard|
 |225|[Implement Stack using Queue](https://leetcode.com/problems/implement-stack-using-queues/) | [Swift](./algorithms/swift/implementStackUsingQueue/implementStackUsingQueue.swift)|Easy|
 |230|[Kth Smallest Element in a BST](https://leetcode.com/problems/kth-smallest-element-in-a-bst/) | [Swift](./algorithms/swift/kthSmallestElementInABST/kthSmallestElementInABST.swift)|Medium|
---
+++ algorithms/swift/kthLargestElementInAnArray/kthLargestElementInAnArray.swift
@@ -0,0 +1,75 @@
+/*
+
+ 215. Kth Largest Element in an Array
+
+
+ Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ Example 1:
+
+ Input: [3,2,1,5,6,4] and k = 2
+ Output: 5
+
+
+ Example 2:
+
+ Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ Output: 4
+
+ Note:
+
+ You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+
+ https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+
+func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        var nums = nums
+        var start = 0
+        var end = nums.count-1
+
+        var index = nums.count-k
+        while(start <= end){
+            var position = partition(&nums, start, end)
+
+            if(position == index){
+                return nums[position]
+            }else if(position < index){
+                start = position + 1
+            }else{
+                end = position - 1
+            }
+        }
+        return -1
+    }
+
+func partition(_ nums: inout [Int], _ start: Int, _ end:  Int) -> Int{
+    var index = start
+    var pivot = nums[start]
+    var start = start
+    var end = end
+
+    while(start <= end){
+
+        while(start <= end && nums[start] <= pivot){
+            start += 1
+        }
+        while(start <= end && nums[end] >= pivot){
+            end -= 1
+        }
+        if(start <= end){
+            swap(&nums, start, end)
+            start += 1
+            end -= 1
+        }
+    }
+    swap(&nums, index, end)
+    return end
+}
+
+func swap(_ nums: inout [Int], _ start: Int, _ end: Int){
+    let temp = nums[start]
+    nums[start]  = nums[end]
+    nums[end] = temp
+}
