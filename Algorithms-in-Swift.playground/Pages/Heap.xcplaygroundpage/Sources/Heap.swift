d32789ffe1a0cd5d38a1ac3b6d330474e1da1c46
AnYuan <donganyuan1010@gmail.com>
2019-07-03 09:44:54 +0800

    add Heap implementation

---
+++ Algorithms-in-Swift.playground/Pages/Heap.xcplaygroundpage/Sources/Heap.swift
@@ -0,0 +1,128 @@
+struct Heap<Element: Equatable> {
+    var elements: [Element] = []
+    let sort: (Element, Element) -> Bool
+    
+    init(sort: @escaping (Element, Element) -> Bool,
+         elements: [Element] = []) {
+        self.sort = sort
+        self.elements = elements
+        
+        if !elements.isEmpty {
+            for i in stride(from: elements.count / 2 - 1, through: 0, by: -1) {
+                siftDown(from: i)
+            }
+        }
+    }
+    
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    var count: Int {
+        return elements.count
+    }
+    
+    func peek() -> Element? {
+        return elements.first
+    }
+    
+    func leftChildIndex(ofParentAt index: Int) -> Int {
+        return (2 * index) + 1
+    }
+    
+    func rightCHildIndex(ofParentAt index: Int) -> Int {
+        return (2 * index) + 2
+    }
+    
+    func parentIndex(ofChildAt index: Int) -> Int {
+        return (index - 1) / 2
+    }
+    // remove
+    mutating func remove() -> Element? {
+        guard !isEmpty else { return nil }
+        elements.swapAt(0, count - 1)
+        defer {
+            siftDown(from: 0)
+        }
+        return elements.removeLast()
+    }
+    
+    mutating func siftDown(from index: Int) {
+        var parent = index
+        while true {
+            let left = leftChildIndex(ofParentAt: parent)
+            let right = rightCHildIndex(ofParentAt: parent)
+            var candidate = parent
+            
+            // if there is a left child, and it has a higher priority than
+            // its parent, make it the candidate.
+            if left < count && sort(elements[left], elements[candidate]) {
+                candidate = left
+            }
+            // if there is a right child, and it has an even greater priority,
+            // it will become the candidate instead.
+            if right < count && sort(elements[right], elements[candidate]) {
+                candidate = right
+            }
+            // if candidate is still parent, you have reached the end
+            // no more sifting is required.
+            if candidate == parent {
+                return
+            }
+            
+            elements.swapAt(parent, candidate)
+            parent = candidate
+        }
+    }
+    // index
+    mutating func insert(_ element: Element) {
+        elements.append(element)
+        siftUp(from: elements.count - 1)
+    }
+    
+    mutating func siftUp(from index: Int) {
+        var child = index
+        var parent = parentIndex(ofChildAt: child)
+        while child > 0 && sort(elements[child], elements[parent]) {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = parentIndex(ofChildAt: child)
+        }
+    }
+    
+    mutating func remove(at index: Int) -> Element? {
+        guard index < elements.count else {
+            return nil
+        }
+        if index == elements.count - 1 {
+            return elements.removeLast()
+        } else {
+            elements.swapAt(index, elements.count - 1)
+            defer {
+                siftDown(from: index)
+                siftUp(from: index)
+            }
+            return elements.removeLast()
+        }
+    }
+    
+    // search
+    func index(of element: Element, startingAt i: Int) -> Int? {
+        if i >= count {
+            return nil
+        }
+        if sort(element, elements[i]) {
+            return nil
+        }
+        if element == elements[i] {
+            return i
+        }
+        if let j = index(of: element, startingAt: leftChildIndex(ofParentAt: i)) {
+            return j
+        }
+        if let j = index(of: element, startingAt: rightCHildIndex(ofParentAt: i)) {
+            return j
+        }
+        return nil
+    }
+}
