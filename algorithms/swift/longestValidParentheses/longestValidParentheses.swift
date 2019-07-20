32a71db72138cbd22d25425d5bb0255017121b2a
AnYuan <donganyuan1010@gmail.com>
2019-07-20 10:44:03 +0800

    update

--- algorithms/swift/longestValidParentheses/longestValidParentheses.swift
+++ algorithms/swift/longestValidParentheses/longestValidParentheses.swift
@@ -15,12 +15,13 @@ Input: ")()())"
 Output: 4
 Explanation: The longest valid parentheses substring is "()()"
 
+https://leetcode.com/problems/longest-valid-parentheses/
 */
 func longestValidParentheses(_ s: String) -> Int {
-    
+
     let n = s.count
     guard n > 1 else { return 0 }
-    
+
     var dp = Array(repeating: 0, count: n)
     let chars = Array(s)
     var left = 0 //num of left paren
@@ -31,7 +32,7 @@ func longestValidParentheses(_ s: String) -> Int {
         }
         if chars[i] == ")" && left > 0 {
             dp[i] = dp[i-1] + 2
-            // last valid paren 
+            // last valid paren
             if i - dp[i] > 0 {
                 dp[i] = dp[i] + dp[i-dp[i]]
             }
